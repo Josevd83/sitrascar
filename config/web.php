fdcde6a3f846768ddd5327fbc7351b5a408dae8e
Richard <rsilva@banavih.gob.ve>
2017-06-30 14:28:24 -0400

    configurando yii2 admin en config/web.php

--- config/web.php
+++ config/web.php
@@ -6,6 +6,15 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language' => 'es',
+    'modules' => [
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+	    'layout' => 'left-menu',
+	    'mainLayout' => '@app/views/layouts/main.php',
+	    'defaultRoute' => 'user',
+        ]
+    ],
     'components' => [
         'view' => [
          'theme' => [
@@ -53,6 +62,23 @@ $config = [
             ],
         ],
         */
+	'authManager' => [
+            'class' => 'yii\rbac\PhpManager', // or use 'yii\rbac\DbManager'
+        ]
+    ],
+    'as access' => [
+        'class' => 'mdm\admin\components\AccessControl',
+        'allowActions' => [
+	    '*'
+            //'site/*',
+            //'admin/*',
+            //'some-controller/some-action',
+            // The actions listed here will be allowed to everyone including guests.
+            // So, 'admin/*' should not appear here in the production, of course.
+            // But in the earlier stages of your development, you may probably want to
+            // add a lot of actions here until you finally completed setting up rbac,
+            // otherwise you may not even take a first step.
+        ]
     ],
     'params' => $params,
 ];
